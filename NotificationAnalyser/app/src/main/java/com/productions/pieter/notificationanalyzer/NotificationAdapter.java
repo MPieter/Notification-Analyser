0db20479a46fd1c8df61d53e128f4c1536d8d625
Pieter Meiresone <pieter.meiresone@gmail.com>
2014-09-23 23:42:31 +0200

    Bij notificatie-overzicht weergeven van app-icon.

--- NotificationAnalyser/app/src/main/java/com/productions/pieter/notificationanalyzer/NotificationAdapter.java
+++ NotificationAnalyser/app/src/main/java/com/productions/pieter/notificationanalyzer/NotificationAdapter.java
@@ -1,10 +1,13 @@
 package com.productions.pieter.notificationanalyzer;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
@@ -32,11 +35,18 @@ public class NotificationAdapter extends ArrayAdapter<NotificationView> {
         TextView appName = (TextView) view.findViewById(R.id.app_name);
         TextView appCount = (TextView) view.findViewById(R.id.app_count);
         ProgressBar progressBar = (ProgressBar) view.findViewById(R.id.app_progress_bar);
+        ImageView imageView = (ImageView) view.findViewById(R.id.app_image);
 
         NotificationView nv = this.getItem(position);
         appName.setText(nv.AppName);
         appCount.setText(Integer.toString(nv.Notifications));
         progressBar.setProgress((int) ((double)nv.Notifications / (double)nv.MaxNotifications * 100));
+        try {
+            Drawable icon = view.getContext().getPackageManager().getApplicationIcon(nv.AppName);
+            imageView.setImageDrawable(icon);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
 
         return view;
     }
