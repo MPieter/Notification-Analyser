8680ebcb5f56ae6d44371a025d9242f1822f1180
Pieter Meiresone <pieter.meiresone@gmail.com>
2014-10-26 18:52:28 +0100

    About-activity implementatie

--- NotificationAnalyser/app/src/main/java/com/tierep/notificationanalyser/AboutActivity.java
+++ NotificationAnalyser/app/src/main/java/com/tierep/notificationanalyser/AboutActivity.java
@@ -1,6 +1,9 @@
 package com.tierep.notificationanalyser;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
 
 
 public class AboutActivity extends DrawerActivity {
@@ -12,6 +15,17 @@ public class AboutActivity extends DrawerActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        TextView about_version = (TextView) findViewById(R.id.about_version);
+        if (about_version.isInEditMode()) {
+            about_version.setText("Version 1.0");
+        } else {
+            try {
+                PackageInfo pInfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+                about_version.setText("Version " + pInfo.versionName);
+            } catch (PackageManager.NameNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 }
